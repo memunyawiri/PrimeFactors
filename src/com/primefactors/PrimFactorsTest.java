cfde81f50634c54744a09c110a114ad8db0a46ed
memunyawiri <mutsa.evelyn@hotmail.com>
2017-11-05 19:50:47 +0000

    returning 2 as own prime factor

--- src/com/primefactors/PrimFactorsTest.java
+++ src/com/primefactors/PrimFactorsTest.java
@@ -2,6 +2,7 @@ package com.primefactors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,11 @@ public class PrimFactorsTest {
 	@Test
 	public void oneHasNoPrimeFactors() {
 		assertEquals(Collections.emptyList(), PrimeFactors.computeFactorsFor(1));
+	}
+	
+	@Test
+	public void twoIsItsOwnPrimeFactor() {
+		assertEquals(Arrays.asList(2), PrimeFactors.computeFactorsFor(2));
 	}
 	
 }
--- src/com/primefactors/PrimeFactors.java
+++ src/com/primefactors/PrimeFactors.java
@@ -1,11 +1,17 @@
 package com.primefactors;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
 	
 	public static List<Integer> computeFactorsFor(int i) {
-		return new java.util.ArrayList<Integer>();
+		ArrayList<Integer> factors = new java.util.ArrayList<Integer>();
+		if (i > 1) 
+		{
+			factors.add(2);
+		}
+		return factors;
 	}
 
 }
